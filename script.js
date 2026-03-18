06366900fd6b1492d11faac4cb087d1fc0e7fe66
Vedant Jadhav <121371485+vedant2863@users.noreply.github.com>
2026-03-18 10:16:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,89 @@
 function convertToRoman(num) {
-  const values = [
-    [1000, 'M'],
-    [900, 'CM'],
-    [500, 'D'],
-    [400, 'CD'],
-    [100, 'C'],
-    [90, 'XC'],
-    [50, 'L'],
-    [40, 'XL'],
-    [10, 'X'],
-    [9, 'IX'],
-    [5, 'V'],
-    [4, 'IV'],
-    [1, 'I']
-  ];
+  	const obj = {
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
 
-  let result = '';
+	let result = '';
 
-  for (let [value, symbol] of values) {
-    while (num >= value) {
-      result += symbol;
-      num -= value;
-    }
-  }
+	for (let i = 0; i <= 6; i++) {
+		let [symbol, value] = obj[i];
 
-  return result;
+		// Handle subtractive cases
+		if (value === 1000) {
+			while (num >= 1000) {
+				result += 'M';
+				num -= 1000;
+			}
+		}
+		else if (value === 500) {
+			if (num >= 900) {
+				result += 'CM';
+				num -= 900;
+			}
+			while (num >= 500) {
+				result += 'D';
+				num -= 500;
+			}
+			if (num >= 400) {
+				result += 'CD';
+				num -= 400;
+			}
+		}
+		else if (value === 100) {
+			while (num >= 100) {
+				result += 'C';
+				num -= 100;
+			}
+		}
+		else if (value === 50) {
+			if (num >= 90) {
+				result += 'XC';
+				num -= 90;
+			}
+			while (num >= 50) {
+				result += 'L';
+				num -= 50;
+			}
+			if (num >= 40) {
+				result += 'XL';
+				num -= 40;
+			}
+		}
+		else if (value === 10) {
+			while (num >= 10) {
+				result += 'X';
+				num -= 10;
+			}
+		}
+		else if (value === 5) {
+			if (num >= 9) {
+				result += 'IX';
+				num -= 9;
+			}
+			while (num >= 5) {
+				result += 'V';
+				num -= 5;
+			}
+			if (num >= 4) {
+				result += 'IV';
+				num -= 4;
+			}
+		}
+		else if (value === 1) {
+			while (num >= 1) {
+				result += 'I';
+				num -= 1;
+			}
+		}
+	}
+
+	return result;
 }
 
 module.exports = convertToRoman;
